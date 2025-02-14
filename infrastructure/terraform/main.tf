f31263996d67909554e6a3b948ba25906ea4f335
rezawq <rezawq@gmail.com>
2025-02-14 15:15:10 +0300

    fixup! 3.1 [terraform] add compute resourses and jump server

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -34,7 +34,7 @@ resource "yandex_storage_bucket" "data_bucket" {
   bucket        = "${var.yc_bucket_name}-${var.yc_folder_id}"
   access_key    = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key    = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  force_destroy = true
+  force_destroy = false
   # default_storage_class = "COLD"
   anonymous_access_flags {
     read = true
